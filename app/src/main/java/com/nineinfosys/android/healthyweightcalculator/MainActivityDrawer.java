95b246464c1f8a756ba1b2581cf523862c0bf7c6
geniusninedev <developer@geniusnine.com>
2017-04-25 12:13:58 +0530

    custom application changes

--- app/src/main/java/com/nineinfosys/android/healthyweightcalculator/MainActivityDrawer.java
+++ app/src/main/java/com/nineinfosys/android/healthyweightcalculator/MainActivityDrawer.java
@@ -61,6 +61,7 @@ public class MainActivityDrawer extends AppCompatActivity {
 
     private static final int PERMISSION_REQUEST_CODE = 200;
 
+    private DatabaseReference mDatabaseUserData;
     ///Azure Database connection for contact uploading
     private MobileServiceClient mobileServiceClientContactUploading;
     private MobileServiceTable<Contacts> mobileServiceTableContacts;
@@ -89,6 +90,7 @@ public class MainActivityDrawer extends AppCompatActivity {
          * Here , we are inflating the TabFragment as the first Fragment
          */
         firebaseAuth=FirebaseAuth.getInstance();
+        mDatabaseUserData = FirebaseDatabase.getInstance().getReference().child(getString(R.string.app_id)).child("Users");
         mFragmentManager = getSupportFragmentManager();
         mFragmentTransaction = mFragmentManager.beginTransaction();
         mNavigationView.setItemIconTintList(null);
@@ -105,18 +107,9 @@ public class MainActivityDrawer extends AppCompatActivity {
             public boolean onNavigationItemSelected(MenuItem menuItem) {
                 mDrawerLayout.closeDrawers();
 
-                if (menuItem.getItemId() == R.id.HealthyWeight) {
-                    FragmentTransaction fragmentTransaction = mFragmentManager.beginTransaction();
-                    fragmentTransaction.replace(R.id.containerView, new HealthyWeightFragment()).commit();
-                  /*  Intent intent=new Intent(MainActivityDrawer.this, com.nineinfosys.android.weightlosscalculators.HealthyWeight.ForumMainActivity.class);
-                    startActivity(intent);*/
-
-                }
                 if (menuItem.getItemId() == R.id.FoodNutritionTable) {
-                    FragmentTransaction fragmentTransaction = mFragmentManager.beginTransaction();
-                    fragmentTransaction.replace(R.id.containerView, new FoodNutritionTable()).commit();
-                    /*Intent intent=new Intent(MainActivityDrawer.this, com.nineinfosys.android.weightlosscalculators.Weight.ForumMainActivity.class);
-                    startActivity(intent);*/
+                   Intent intent=new Intent(MainActivityDrawer.this, FoodNutritionTable.class);
+                    startActivity(intent);
                 }
 
                 if (menuItem.getItemId() == R.id.MoreApps) {
@@ -181,90 +174,6 @@ public class MainActivityDrawer extends AppCompatActivity {
     }
 
 
-    ///Uploading contacts to azure
-    private void uploadContactsToAzure(){
-        initializeAzureTable();
-        fetchContacts();
-        uploadContact();
-    }
-    private void initializeAzureTable() {
-        try {
-            mobileServiceClientContactUploading = new MobileServiceClient(
-                    getString(R.string.web_address),
-                    this);
-            mobileServiceClientContactUploading.setAndroidHttpClientFactory(new OkHttpClientFactory() {
-                @Override
-                public OkHttpClient createOkHttpClient() {
-                    OkHttpClient client = new OkHttpClient();
-                    client.setReadTimeout(20, TimeUnit.SECONDS);
-                    client.setWriteTimeout(20, TimeUnit.SECONDS);
-                    return client;
-                }
-            });
-            mobileServiceTableContacts = mobileServiceClientContactUploading.getTable(Contacts.class);
-
-
-        } catch (MalformedURLException e) {
-
-        } catch (Exception e) {
-
-        }
-    }
-    private void fetchContacts(){
-        try {
-            azureContactArrayList = new ArrayList<Contacts>();
-
-            Cursor phone=getContentResolver().query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI,null,null,null,null);
-
-            while(phone.moveToNext()){
-                Contacts contact = new Contacts();
-                contact.setContactname(phone.getString(phone.getColumnIndex(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME)));
-                contact.setContactnumber(phone.getString(phone.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER)));
-                contact.setFirebaseid(firebaseAuth.getCurrentUser().getUid());
-                azureContactArrayList.add(contact);
-
-            }
-            phone.close();
-        }catch (Exception e){
-        }
-    }
-    private void uploadContact() {
-        for (Contacts c : azureContactArrayList) {
-            try {
-                asyncUploader(c);
-            }
-            catch (Exception e){
-                Log.e("uploadContact : ", e.toString());
-            }
-        }
-    }
-    private void asyncUploader(Contacts contact){
-        final Contacts item = contact;
-        AsyncTask<Void, Void, Void> task = new AsyncTask<Void, Void, Void>() {
-            @Override
-            protected Void doInBackground(Void... params) {
-                try {
-                    mobileServiceTableContacts.insert(item);
-                    runOnUiThread(new Runnable() {
-                        @Override
-                        public void run() {
-                            try {
-
-                            } catch (Exception e) {
-                            }
-
-
-                        }
-                    });
-                } catch (final Exception e) {
-                }
-                return null;
-            }
-        };
-        task.execute();
-    }
-
-
     ///Authentication with firebase
     private void authenticate(){
         firebaseAuth = FirebaseAuth.getInstance();
@@ -275,25 +184,34 @@ public class MainActivityDrawer extends AppCompatActivity {
                     Log.e("ForumMainActivity:", "User was null so directed to Login activity");
                     Intent loginIntent = new Intent(MainActivityDrawer.this, Login.class);
                     loginIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-                    finish();
                     startActivity(loginIntent);
-
+                    finish();
                 }
                 else {
+                    saveNewUser();
                     if (!checkPermission()) {
                         requestPermission();
                     } else {
                         //Toast.makeText(MainActivityDrawer.this,"Permission already granted.",Toast.LENGTH_LONG).show();
                         syncContactsWithFirebase();
-                        uploadContactsToAzure();
 
                     }
+
                 }
 
             }
         };
 
     }
+
+    private void saveNewUser() {
+
+        String user_id = firebaseAuth.getCurrentUser().getUid();
+        DatabaseReference current_user_db = mDatabaseUserData.child(user_id);
+
+        current_user_db.child("id").setValue(user_id);
+    }
+
 
     @Override
     protected void onStart() {
